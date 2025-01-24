9af72f1c0e8eada3d1101d1ffe082704996d6be8
Rakesh Kuamr <rakeshdhiman644@github.com>
2025-01-24 20:07:34 +0530

    new code

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+	$('.image-slider').slick({
+				dots: false, // Add navigation dots if desired
+				arrows: false, // Disable default slick slider arrows
+				autoplay: false, // Set to true for autoplay
+				autoplaySpeed: 2000 // Set the speed of autoplay
+			});
+});
+
+		// Custom slide navigation functions
+function prevSlide() {
+	$('.image-slider').slick('slickPrev');
+}
+
+function nextSlide() {
+	$('.image-slider').slick('slickNext');
+}
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+document.getElementById('year').innerText = currentYear;
